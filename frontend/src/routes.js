0c2e4328f22c6bfec4f7da6815e6056cefce8ca2
EmsRibeiro <jrc.emerson@gmail.com>
2025-08-18 14:05:18 -0300

    add profile folder

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Logon/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Logon />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
