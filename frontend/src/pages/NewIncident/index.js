2399d1b0e43b880944c4950f3f3e448d51ce3403
EmsRibeiro <jrc.emerson@gmail.com>
2025-08-18 18:46:37 -0300

    Create New Incident folder. Frontend ready to join backend

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './styles.css';
+import logoImg from "../../assets/logo.svg"; 
+import { FiArrowLeft, FiLogIn } from "react-icons/fi";
+
+export default function NewIncident() {
+  return (
+    <div className="new-incident-container">
+          <div className="content">
+            <section>
+              <img src={logoImg} alt="Escrita Be The Hero" />
+    
+              <h1>Cadastrar novo caso</h1>
+              <p>
+                Descreva o caso detalhadamente para encontrar um herói para salvar sua dia!
+              </p>
+              <Link className="back-link" to="/profile">
+                <FiArrowLeft size={17} color="#E02041" />
+                Voltar para Home.
+              </Link>
+            </section>
+    
+            <form>
+              <input placeholder="Título do caso" />
+              <textarea placeholder="Descrição" />
+              <input placeholder="Valor em Reais"/>
+
+              <button className="button" type="submit">Cadastrar</button>
+            </form>
+          </div>
+        </div>
+  )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Logon />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/incidents/new" element={<NewIncident />} />
       </Routes>
     </BrowserRouter>
   );
